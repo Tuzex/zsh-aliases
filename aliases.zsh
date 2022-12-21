02f84c59c195780e7236172267cef5487a2b0d8f
Edo Barnas <iam@edobarnas.com>
2022-12-21 09:51:17 +0100

    Use `"$(pwd)"` instead of `$(pwd)`

--- aliases.zsh
+++ aliases.zsh
@@ -6,65 +6,65 @@ alias python='docker run --rm -it -u $UID:$UID python:3-alpine python'
 # PHP
 ###############
 
-alias php73='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tuzex/php:7.3-cli php'
-alias php74='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tuzex/php:7.4-cli php'
-alias php8='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tuzex/php:8.0-cli php'
-alias php81='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tuzex/php:8.1-cli php'
-alias php82='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tuzex/php:8.2-cli php'
+alias php73='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tuzex/php:7.3-cli php'
+alias php74='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tuzex/php:7.4-cli php'
+alias php8='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tuzex/php:8.0-cli php'
+alias php81='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tuzex/php:8.1-cli php'
+alias php82='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tuzex/php:8.2-cli php'
 alias php="php82"
 
 ## Debuging
 
-alias php8x='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tuzex/php:8.0-cli--dev php'
-alias php81x='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tuzex/php:8.1-cli--dev php'
-alias php82x='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tuzex/php:8.2-cli--dev php'
+alias php8x='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tuzex/php:8.0-cli--dev php'
+alias php81x='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tuzex/php:8.1-cli--dev php'
+alias php82x='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tuzex/php:8.2-cli--dev php'
 alias phpx="php82x"
 
 ## Dependency
 
-alias composer1='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tuzex/composer:1 composer'
-alias composer='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tuzex/composer:2 composer'
+alias composer1='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tuzex/composer:1 composer'
+alias composer='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tuzex/composer:2 composer'
 
 ## Security
 
-alias security-checker='docker run --rm -it -v $(pwd):/app -w /app symfonycorp/cli check:security'
+alias security-checker='docker run --rm -it -v "$(pwd)":/app -w /app symfonycorp/cli check:security'
 
 ## Testing
 
-alias phpunit='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tuzex/php:8.2-cli vendor/bin/phpunit -c phpunit.xml'
-alias phpstan='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tuzex/php:8.2-cli vendor/bin/phpstan analyse -c phpstan.neon --memory-limit=-1'
-alias phpmd='docker run --rm -it -v $(pwd):/app -w /app -u 1000:1000 phpqa/phpmd'
-alias codecept='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tuzex/php:8.2-cli vendor/bin/codecept'
-alias ecs='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tuzex/php:8.2-cli vendor/bin/ecs check -c ecs.php'
+alias phpunit='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tuzex/php:8.2-cli vendor/bin/phpunit -c phpunit.xml'
+alias phpstan='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tuzex/php:8.2-cli vendor/bin/phpstan analyse -c phpstan.neon --memory-limit=-1'
+alias phpmd='docker run --rm -it -v "$(pwd)":/app -w /app -u 1000:1000 phpqa/phpmd'
+alias codecept='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tuzex/php:8.2-cli vendor/bin/codecept'
+alias ecs='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tuzex/php:8.2-cli vendor/bin/ecs check -c ecs.php'
 
 # NODE
 ###############
 
-alias node='docker run --rm -it -v $(pwd):/app:delegated -w /app -u $UID:$UID node:16'
+alias node='docker run --rm -it -v "$(pwd)":/app:delegated -w /app -u $UID:$UID node:16'
 
 ## Dependency
 
 alias npm='dnode npm'
-alias npm:8080='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID -p 8080:8080 node:16 npm'
-alias npm:3000='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID -p 3000:3000 node:16 npm'
+alias npm:8080='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID -p 8080:8080 node:16 npm'
+alias npm:3000='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID -p 3000:3000 node:16 npm'
 
 alias npx='dnode npx'
-alias npx:1337='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID -p 8080:8080 node:16 npx'
+alias npx:1337='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID -p 8080:8080 node:16 npx'
 
 alias yarn='dnode yarn'
-alias yarn:8080='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID -p 8080:8080 node:16 yarn'
-alias yarn:3000='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID -p 3000:3000 node:16 yarn'
+alias yarn:8080='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID -p 8080:8080 node:16 yarn'
+alias yarn:3000='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID -p 3000:3000 node:16 yarn'
 
 # TYPESCRIPT
 ###############
 
-alias tsc='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID tsc:latest tsc'
+alias tsc='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID tsc:latest tsc'
 
 # VUE
 ###############
 
-alias vue='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID vue:latest vue'
-alias vui='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID -p 8000:8000 vue:latest vue ui --headless --port 8000 --host 0.0.0.0'
+alias vue='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID vue:latest vue'
+alias vui='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID -p 8000:8000 vue:latest vue ui --headless --port 8000 --host 0.0.0.0'
 
 # DOCKER
 ###############
@@ -86,7 +86,7 @@ alias portainer='docker run --name portainer --restart=always -d -p 9123:9000 -v
 # TERRAFORM
 ###############
 
-alias tf='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID hashicorp/terraform:light'
+alias tf='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID hashicorp/terraform:light'
 
 # TOOLS
 ###############
@@ -101,4 +101,4 @@ alias glyphy='~/.docker-glyphhanger/docker-glyphhanger.sh'
 
 ## Json Server
 
-alias jsonserver='docker run --rm -it -v $(pwd):/app -w /app -u $UID:$UID node:16 npm'
+alias jsonserver='docker run --rm -it -v "$(pwd)":/app -w /app -u $UID:$UID node:16 npm'
